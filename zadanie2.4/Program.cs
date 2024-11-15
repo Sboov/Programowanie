c8dc35c7699a15493ccdd920ef1ee3729390845a
Sboov <patryk.padol@gmail.com>
2024-11-15 16:12:40 +0100

    1.0
    ex 2.4

---
+++ zadanie2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace zadanie2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoją średnia ocen a ja sprawdzę przysługującą kwote stypendium");
+            double srednia = double.Parse(Console.ReadLine());
+            if (srednia >= 2.00 && srednia <= 4.00)
+            {
+                Console.WriteLine("Nie przeysługuje Ci stypendium");
+            }
+            if (srednia >= 4.01 && srednia <= 4.80)
+            {
+                Console.WriteLine("Przysługuje Ci 300,00 zł stypendium");
+
+            }
+            if (srednia >= 4.81 && srednia <= 5.00)
+                {
+                Console.WriteLine("Przysługuje Ci 600,00 zł stypendium");
+            }
+
+        }
+    }
+}
